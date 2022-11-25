d53a4cd8fd0ebd69f96fc9638c2e3cf29998c0ba
Lech Gudalewicz <lechgu@gmail.com>
2022-11-25 10:06:56 -0800

    pages

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -3,11 +3,13 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Frontend;
 using Microsoft.Extensions.DependencyInjection;
 using System.Net.Http;
+using Havit.Blazor.Components.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddHxServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
